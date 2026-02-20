66e71db90dc29ba3ba319d87819fd2ad403d90b6
瑞井 箕月 <6161.k.iga@gmail.com>
2026-02-20 23:08:26 +0900

    ページタイトルを追加

--- lunanthus/routes.js
+++ lunanthus/routes.js
@@ -1,11 +1,11 @@
 export default [
-    { path: "/", file: "/pages/html/home/home.html", style: null },
-    { path: "/lottery", file: "/pages/html/lottery/lottery.html", script: "/pages/js/lottery/index.js", style: null },
-    { path: "/calculator", file: "/pages/html/calculator/calculator.html", script: "/pages/js/calculator/script.js", style: "/pages/css/calculator/style.css" },
-    { path: "/ip", file: "/pages/html/ip/ip.html", script: "/pages/js/ip/ip.js", style: null },
-    { path: "/image_conversion", file: "/pages/html/image_conversion/image_conversion.html", script: "/pages/js/image_conversion/script.js", style: null },
-    { path: "/password_generator", file: "/pages/html/password_generator/password_generator.html", script: "/pages/js/password_generator/script.js", style: null },
-    { path: "/qr_generator", file: "/pages/html/qr_generator/qr_generator.html", script: "/pages/js/qr_generator/script.js", style: null },
-    { path: "/exif_frame", file: "/pages/html/exif_frame/exif_frame.html", script: "/pages/js/exif_frame/script.js", style: null },
-    { path: "/about", file: "/pages/html/about/about.html", script: "/pages/js/about/index.js", style: null }
+    { title: null, path: "/", file: "/pages/html/home/home.html", style: null },
+    { title: "抽選", path: "/lottery", file: "/pages/html/lottery/lottery.html", script: "/pages/js/lottery/index.js", style: null },
+    { title: "電卓", path: "/calculator", file: "/pages/html/calculator/calculator.html", script: "/pages/js/calculator/script.js", style: "/pages/css/calculator/style.css" },
+    { title: "アクセス情報確認", path: "/ip", file: "/pages/html/ip/ip.html", script: "/pages/js/ip/ip.js", style: null },
+    { title: "画像変換", path: "/image_conversion", file: "/pages/html/image_conversion/image_conversion.html", script: "/pages/js/image_conversion/script.js", style: null },
+    { title: "パスワード生成", path: "/password_generator", file: "/pages/html/password_generator/password_generator.html", script: "/pages/js/password_generator/script.js", style: null },
+    { title: "QRコード生成", path: "/qr_generator", file: "/pages/html/qr_generator/qr_generator.html", script: "/pages/js/qr_generator/script.js", style: null },
+    { title: "EXIFフレーム", path: "/exif_frame", file: "/pages/html/exif_frame/exif_frame.html", script: "/pages/js/exif_frame/script.js", style: null },
+    { title: "About", path: "/about", file: "/pages/html/about/about.html", script: "/pages/js/about/index.js", style: null }
 ];
