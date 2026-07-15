7564e5d74c388159460335900b23335d4f0de91e
瑞井 箕月 <6161.k.iga@gmail.com>
2026-07-15 23:36:57 +0900

    sitemap自動生成

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import devtools from 'solid-devtools/vite';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import devtools from "solid-devtools/vite";
+import sitemap from "vite-plugin-sitemap";
+import { routes } from "./src/routes";
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-  },
+    plugins: [
+        devtools(),
+        solidPlugin(),
+        tailwindcss(),
+        sitemap({
+            hostname: "https://tools.blossomsarchive.com",
+            routes: routes.map((r) => r.path),
+        } as any),
+    ],
+    server: {
+        port: 3000,
+    },
+    build: {
+        target: "esnext",
+    },
 });
